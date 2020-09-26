43ba1945bede87f22f842651712d10a633fbc51a
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-09-26 19:49:46 +0900

    Finish 01_basic_settings ver1

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,13 @@ module RunteqNormal
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test false
+      g.routes false
+    end
   end
+
 end
