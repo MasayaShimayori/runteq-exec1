71dde216136e3b9571bbbe4759a7958ea867703c
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-10-06 23:08:25 +0900

    1-try 04_basic

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.6.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+gem 'sorcery'
+gem 'pry-byebug'
+
 gem 'font-awesome-sass'
 gem 'bootstrap', '~>4.1.1'
 gem 'jquery-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,11 @@
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require_tree .
+//= require jquery3
+
