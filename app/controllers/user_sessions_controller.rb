eefed6c68db6c5e91ba45f3b197d13e69f4a507b
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-10-08 00:25:44 +0900

    1.5try 04_basic

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,6 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create]
-  def new
-  end
+  skip_before_action :require_login, only: %i(:new, :create)
+  def new; end
 
   def create
     @user = login(params[:email], params[:password])
@@ -10,7 +9,7 @@ class UserSessionsController < ApplicationController
       redirect_to(root_path, notice: 'Login successful')
     else
       flash.now[:alert] = 'Login failed'
-      render action = 'new'
+      render 'new'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   resources :users
-  get 'login' => 'user_sessions#new' , :as => :login
+  get 'login' => 'user_sessions#new', :as => :login
   post 'login' => 'user_sessions#create'
   post 'logout' => 'user_sessions#destroy', :as => :logout
 end
