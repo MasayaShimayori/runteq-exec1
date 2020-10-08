bb37b8965c2895c4e48e3c2a48a530122a283786
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-10-09 01:00:17 +0900

    1.9try 04_basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i(:show, :edit, :update, :destroy)
-  skip_before_action :require_login, only: %i(:index, :new, :create)
+  before_action :set_user, only: %i(show edit update destroy) 
+  skip_before_action :require_login, only: %i(index new create)
 
   # GET /users
   # GET /users.json
@@ -62,12 +62,10 @@ class UsersController < ApplicationController
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
