38e00ac472d45a0938cb70720ad18d5b02aff4c6
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-10-09 01:26:59 +0900

    2try 04_basic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :last_name, length: { maximum: 255 }
+  validates :first_name, length: { maximum: 255 }, presence: true
+  validates :last_name, length: { maximum: 255 }, presence: true
   validates :password, length: { minimum: 3 } # , if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true # , if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true
