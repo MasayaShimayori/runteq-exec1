bcb2abffb2091106505d96fd857afed929a63a7e
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-10-12 00:17:25 +0900

    1try 05_basic

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,10 @@ module RunteqNormal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :ja # デフォルトを日本語に
+    # 翻訳ファイルの読み込み用、パス設定
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}' ).to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
