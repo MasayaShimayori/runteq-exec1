7e97f088a9e3bc5d5da85c1f7cc8e0e2ac852508
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-10-20 00:24:54 +0900

    4try 07_basic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,7 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login
   def top
     user = User.find_by(params[:id])
-    if user
-      @user = UserDecorator.new(user)
-    end
+    return unless  user
+    @user = UserDecorator.new(user)
   end
 end
