991ae58bc247fc5bc8662b8b9f409e23da046cb1
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-11-02 21:13:19 +0900

    4try 08_basic

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,6 @@
 class Board < ApplicationRecord
   validates :title, length: { maximum: 255 }, presence: true
   validates :body, length: { maximum: 65_535 }, presence: true
-  validates :user_id, presence: true
 
   belongs_to :user
 end
