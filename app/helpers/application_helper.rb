1dbcecd226cd03a13bca54f8aae1f512e6ef9e92
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2020-12-24 18:46:53 +0900

    retry 2nd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    basic_title = "RUNTEQ BOARD APP"
+    basic_title = 'RUNTEQ BOARD APP'
     if page_title.empty?
       basic_title
     else
