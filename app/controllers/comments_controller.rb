9437b3abbcfa15b1ad2d7eee41e9945a0c9e5cb4
Masaya Shimayori <masayashimayori@MasayanoMacBook-Pro.local>
2021-01-05 20:39:55 +0900

    1st-try

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,23 @@ class CommentsController < ApplicationController
   def index; end
 
   def create
-    @comment = current_user.comments.build(comment_params)
+    @comment = current_user.comments.create(comment_params)
     @board = Board.find_by(id: params[:board_id])
-    if @comment.save
-      redirect_to @board, success: 'コメントを作成できました'
-    else
-      redirect_to @board, danger: 'コメントを作成できませんでした'
+    respond_to do |format|
+    format.html {
+      if @comment.save
+        redirect_to @board, success: 'コメントを作成できました'
+      else
+        redirect_to @board, danger: 'コメントを作成できませんでした'
+      end }
+    format.js
     end
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+    @comment = @board.comments.find(params[:format])
+    @comment.destroy
   end
 
   private
